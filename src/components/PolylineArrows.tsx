3023a8bd8431dfb01b9c5a32225a4ccdd3ea17cd
David Gonzalez <dav6wat@gmail.com>
2026-01-10 23:54:05 +0100

    change current track style color

--- src/components/PolylineArrows.tsx
+++ src/components/PolylineArrows.tsx
@@ -2,9 +2,6 @@ import type { LatLngTuple } from 'leaflet';
 import { useEffect, useRef } from 'react';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
-import { HighlightablePolyline } from 'leaflet-highlightable-layers';
-import { type RunTypes } from './PolylineCustom';
-import { borderColor } from '../helpers/colors';
 
 interface Props {
   positions: LatLngTuple[];
@@ -29,37 +26,34 @@ export const PolylineArrows = ({ positions, difficulty }: Props): null => {
       isPanesCreated.current = true;
     }
 
-    const polyline = new HighlightablePolyline(positions, {
-      color: '#ff00FF',
+    const polyline = L.polyline(positions, {
+      color: '#ffffff80',
       weight: 6,
-      raised: false,
-      outlineWeight: 10,
       interactive: false,
-      outlineColor: borderColor(difficulty as RunTypes),
       pane: 'current-track',
     });
 
-    const polylineArrows = L.polyline(positions, {
-      color: 'transparent',
-      weight: 6,
-      interactive: false,
-      pane: 'current-arrows',
-    }).arrowheads({
-      yawn: 45,
-      frequency: '100m',
-      fill: true,
-      color: '#ffff00',
-      weight: 1,
-      size: '6px',
-      lhlZIndex: 401,
-    });
+    // const polylineArrows = L.polyline(positions, {
+    //   color: 'transparent',
+    //   weight: 6,
+    //   interactive: false,
+    //   pane: 'current-arrows',
+    // }).arrowheads({
+    //   yawn: 45,
+    //   frequency: '100m',
+    //   fill: true,
+    //   color: '#ffff00',
+    //   weight: 1,
+    //   size: '6px',
+    //   lhlZIndex: 401,
+    // });
 
     polyline.addTo(map);
-    polylineArrows.addTo(map);
+    // polylineArrows.addTo(map);
 
     return () => {
       polyline.remove();
-      polylineArrows.remove();
+      // polylineArrows.remove();
     };
   }, [map, positions, difficulty]);
 
